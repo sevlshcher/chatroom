f2a5e235097af688bb12b50b79e4766e61a4431a
Sergey Shcherbakov <sevlshcher@gmail.com>
2019-07-08 19:15:41 +0300

    modal, login

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ const socket = io('http://localhost:3000')
 const messageForm = document.getElementById('input-wrapper')
 const messageInput = document.getElementById('message-input')
 const chatList = document.querySelector('.chat-list')
-
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+const modal = document.querySelector('.modal')
+const userForm = document.getElementById('input-wrapper-modal')
+const nameInput = document.getElementById('fullname')
+const nickInput = document.getElementById('nickname')
+const usersOn = document.querySelector('.usersOn')
 
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
 })
 
 socket.on('user-connected', name => {
-    appendMessage(`${name} connected`)
+    console.log(name)
+    appendUser(name)
 })
 
 socket.on('user-disconnected', name => {
-    appendMessage(`${name} disconnected`)
+    appendUser(`${name}`)
+})
+
+userForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const name = nameInput.value
+    const nick = nickInput.value
+    if (name === '' || nick === '') {
+        alert('Незаполненное поле формы')
+    } else {
+        socket.emit('new-user', name)
+        modal.style.display = 'none'
+        nameInput.value = ''
+        nickInput.value = ''
+    }
 })
 
 messageForm.addEventListener('submit', e => {
@@ -31,4 +47,10 @@ function appendMessage(message) {
     const chatElement = document.createElement('div')
     chatElement.innerText = message
     chatList.append(chatElement)
+}
+
+function appendUser(name) {
+    const user = document.createElement('li')
+    usersOn.innerText = name
+    usersOn.append(user)
 }
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const io = require('socket.io')(3000)
 
 const users = {}
+const connections = []
 
 io.on('connection', socket => {
+    connections.push(socket)
     socket.on('new-user', name => {
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)
@@ -13,5 +15,6 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[socket.id]
+        connections.splice(connections.indexOf(socket), 1)
     })
 })
